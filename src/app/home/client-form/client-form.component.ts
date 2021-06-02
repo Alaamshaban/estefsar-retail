e1783828122ef296755c61660afa461773f8f193
Alaa-shaban <ashabaan@cloudinn.net>
2021-06-02 23:36:15 +0200

    modify input type file

--- src/app/home/client-form/client-form.component.ts
+++ src/app/home/client-form/client-form.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Select } from '@ngxs/store';
+import { Actions, ofActionSuccessful, Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Category } from 'src/app/shared/models/category.model';
 import { QuestionGroup } from 'src/app/shared/models/question.model';
+import { CategoriesService } from 'src/app/shared/services/categories.service';
+import { SetQuestions } from 'src/app/store/questions/question.actions';
 import { QuestionsState, QuestionStateModel } from 'src/app/store/questions/question.state';
 
 @Component({
@@ -12,10 +16,26 @@ import { QuestionsState, QuestionStateModel } from 'src/app/store/questions/ques
 export class ClientFormComponent implements OnInit {
   @Select(QuestionsState.getQuestions)
   groups$: Observable<QuestionStateModel>;
+  categories$: Observable<Category[]>;
+  types = ['Family', 'Individual'];
 
-  constructor() { }
+  constructor(
+    private store: Store,
+    private categoriesService: CategoriesService) { }
 
   ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories(): void {
+    this.categories$ = this.categoriesService.getCategories()
+      .pipe(map(
+        (result: object) => result['policy_categories']
+      ));
+  }
+
+  search(filterValues): void {
+    this.store.dispatch(new SetQuestions(filterValues));
   }
 
 }
--- src/app/shared/components/dynamic-form-question/dynamic-form-question.component.ts
+++ src/app/shared/components/dynamic-form-question/dynamic-form-question.component.ts
@@ -7,7 +7,7 @@ import { Question, QuestionGroup } from '../../models/question.model';
   templateUrl: './dynamic-form-question.component.html',
   styleUrls: ['./dynamic-form-question.component.scss']
 })
-export class DynamicFormQuestionComponent  {
+export class DynamicFormQuestionComponent {
 
   @Input() group: QuestionGroup;
   @Input() isSubGroup: boolean;
@@ -22,10 +22,19 @@ export class DynamicFormQuestionComponent  {
     return this.group.form.controls;
   }
 
+  onFileChange(event, ctrl): void {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      const formData = new FormData();
+      this.group.form.patchValue({
+        [`${ctrl}_file`]: formData.append('file', file)
+      });
+    }
+  }
   get GroupTitle(): string {
     if (!this.isSubGroup) {
       return this.group.questions[0].field_sets[0].title;
-    } else{
+    } else {
       return this.group.title;
     }
   }
